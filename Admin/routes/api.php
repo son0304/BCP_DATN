5226b828c19b7d856e9972a9d14c8cc78b39388f
tienThuong <tienThuong@gmail.com>
2025-10-12 15:13:46 +0700

    api đăng ký

--- Admin/routes/api.php
+++ Admin/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\VenueApiController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/venues', [VenueApiController::class, 'index']);
+// API Route cho đăng ký
+Route::post('/auth/register', [AuthController::class, 'register']);
+

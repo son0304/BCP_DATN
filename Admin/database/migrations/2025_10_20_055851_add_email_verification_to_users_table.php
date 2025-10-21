d01be79a01c7d674e09a5bc94fbe26ee04fd04af
Son <sonlew2202@gmail.com>
2025-10-21 14:40:47 +0700

    no commit

--- Admin/database/migrations/2025_10_20_055851_add_email_verification_to_users_table.php
+++ Admin/database/migrations/2025_10_20_055851_add_email_verification_to_users_table.php
@@ -6,25 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_email_verified')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('email_verification_token')->nullable();
-            $table->boolean('is_email_verified')->default(false);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['email_verified_at', 'email_verification_token', 'is_email_verified']);
+            $table->dropColumn(['is_email_verified', 'email_verified_at', 'email_verification_token']);
         });
     }
 };
